8d5cffcc9ded52e5b6ecb9a05f94da3ac903c7fe
Oliver Sier <olli.sier@gmail.com>
2024-10-25 20:57:27 +0100

    E done

--- cw1/E.m
+++ cw1/E.m
@@ -4,19 +4,66 @@ close all
 load("cw1/data/cw1e.mat")
 
 figure
+mesh(reshape(x(:,1),11,11),reshape(x(:,2),11,11),reshape(y,11,11))
+xlabel('X_1')
+ylabel('X_2')
+zlabel('Y')
 
-meanfunc = []; hyp_init.mean = [];
-covfunc = {@covSum, {@covSEard, @covSEard}}; hyp_init.cov = 0.1*randn(6,1);
-likfunc = @likGauss; hyp_init.lik = 0;
+saveas(gcf,'figures/E/raw_data','epsc')
 
-hyp_opt = minimize(hyp_init, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, x, y);
-disp(hyp_opt)
+[xs1, xs2] = meshgrid(linspace(-10, 10, 101), linspace(-10, 10, 101));
+xs = [reshape(xs1, [], 1), reshape(xs2, [], 1)];
 
-[Xs1, Xs2] = meshgrid(linspace(-3, 3, 101), linspace(-3, 3, 101));
+mean_func = []; hyp_init.mean = [];
+lik_func = @likGauss; hyp_init.lik = 0;
 
-[mu, s2] = gp(hyp_opt, @infGaussLik, meanfunc, covfunc, likfunc, x, y, [reshape(Xs1, [], 1), reshape(Xs2, [], 1)]);
+cov_funcs = {
+    {@covSEard},
+    {@covSum, {@covSEard, @covSEard}},
+};
+
+cov_hyp = {
+    0.1*randn(3,1),
+    0.1*randn(6,1),
+};
+
+cov_name = {
+    '@covSEard', 
+    '@covSum, \{@covSEard, @covSEard\}'
+};
 
 figure
-hold on
-surf(Xs1, Xs2, reshape(mu, size(Xs1)))
-scatter3(reshape(x(:,1),11,11),reshape(x(:,2),11,11),reshape(y,11,11), '+')
+
+for i = 1:length(cov_funcs)
+    hyp = hyp_init;
+    hyp.cov = cov_hyp{i};
+    hyp_opt = minimize(hyp, @gp, -100, @infGaussLik, mean_func, cov_funcs{i}, lik_func, x, y);
+    Z_opt = gp(hyp_opt, @infGaussLik, mean_func, cov_funcs{i}, lik_func, x, y);
+    fprintf(cov_name{i})
+    fprintf('Hyper-paramters:\n')
+    fields = fieldnames(hyp_opt);
+    for j = 1:length(fields)
+        field = fields{j};
+        fprintf('%s: %s\n', field, mat2str(round(exp(hyp_opt.(field)), 4)))
+    end
+    fprintf('Log marginal likelihood: %f\n', -Z_opt)
+
+    [mu, s2] = gp(hyp_opt, @infGaussLik, mean_func, cov_funcs{i}, lik_func, x, y, xs);
+
+    subplot(1, length(cov_funcs), i)
+    hold on
+    surf(xs1, xs2, reshape(mu + 2*s2, size(xs1)))
+    surf(xs1, xs2, reshape(mu - 2*s2, size(xs1)))
+    title(cov_name{i})
+
+    view([-37.5 30])
+    xlabel('X_1')
+    ylabel('X_2')
+    zlabel('Y')
+end
+
+f = gcf;
+f.Position = [0, 0, 1000, 420];
+
+saveas(gcf,'figures/E/kernel_comparison','epsc')
+
